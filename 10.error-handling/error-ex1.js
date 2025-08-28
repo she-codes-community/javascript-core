5d835654c4f220e989fa4c5b626a5df8999d1409
Safaa Eek <safaa35@gmail.com>
2025-08-28 19:27:27 +0300

    errors lesson upload

---
+++ 10.error-handling/error-ex1.js
@@ -0,0 +1,20 @@
+// Challenge: find & fix the errors (20 lines)
+function add(a, b) { return a - b; }
+const user = { name: "Alice" };
+console.log(user.profile.city.toLowerCase());
+const jsonText = '{ name: "Bob" }';
+const data = JSON.parse(jsonText);
+let x = 5;
+x();
+if (data.age > 18 {
+  console.log("Adult");
+}
+function average(nums) {
+  let sum = 0;
+  for (let i = 0; i <= nums.length; i++) { sum += nums[i]; }
+  return sum / (nums.length - 1);
+}
+console.log("Average:", average([1,2,3]));
+const msg = "Done;
+console.log(msg);
+export default add
