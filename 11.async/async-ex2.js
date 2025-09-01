35d539fcc5c76912d033ab239e74407336b9be22
Safaa Eek <safaa35@gmail.com>
2025-09-01 22:44:35 +0300

    ex2 async

---
+++ 11.async/async-ex2.js
@@ -0,0 +1,16 @@
+let surpriseTimer;
+
+function scheduleSurprise() {
+  console.log("Surprise scheduled! You have 5 seconds to cancel...");
+  // TODO: setTimeout to log "Surprise!" after 5000 ms
+  // save the id in surpriseTimer
+}
+
+function cancelSurprise() {
+  // TODO: clearTimeout using surpriseTimer
+  console.log("Surprise canceled!");
+}
+
+// Example flow:
+scheduleSurprise();
+// Try calling cancelSurprise() within 5 seconds
